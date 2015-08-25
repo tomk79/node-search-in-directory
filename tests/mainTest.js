dba21c4f068c2c7ea228bf58f0959dccfa0ce2b8
Tomoya Koyanagi <tomk79@gmail.com>
2015-08-25 19:15:54 +0900

    keywordは文字列のみ受け付け可、find() 廃止 (test update)

--- tests/mainTest.js
+++ tests/mainTest.js
@@ -3,27 +3,23 @@ var assert = require('assert');
 var fs = require('fs');
 var SearchInDir = require('../node/main.js');
 
-function getMain( options ){
-	return new SearchInDir;
-}
 
 describe('テキストを検索する', function() {
-	var searchInDir = getMain();
 
 	it("ディレクトリから文字列を検索", function(done) {
-		this.timeout(2*60*1000);
-		searchInDir.find(
+		this.timeout(1*60*1000);
+		var searchInDir = new SearchInDir(
 			[
 				__dirname+'/../**/*'
-				// __dirname+'/data/standard/**/*'
 			] ,
 			{
-				'keyword': /test/i,
+				'keyword': 'test',
 				'filter': [
 					/./i
 				] ,
 				'ignore': [
-					/deep/
+					/deep/,
+					/node_modules/
 				] ,
 				'progress': function( done, max ){
 					console.log( done +'/'+ max + ' done.' );
@@ -42,11 +38,58 @@ describe('テキストを検索する', function() {
 				} ,
 				'complete': function(){
 					console.log('all done!');
-					assert.equal(0, 0);
+					searchInDir = null;
+					assert.equal(1, 1);
 					done();
 				}
 			}
 		);
+	});
+
+});
+
+
+describe('テキスト検索を中止する', function() {
+
+	it("cancel() する", function(done) {
+		this.timeout(1*60*1000);
+		var searchInDir = new SearchInDir(
+			[
+				__dirname+'/../**/*'
+			] ,
+			{
+				'keyword': 'test',
+				'filter': [
+					/./i
+				] ,
+				'progress': function( done, max ){
+					console.log( done +'/'+ max + ' done.' );
+				} ,
+				'match': function( file, result ){
+					// console.log('matche: ' + file);
+					// console.log(result);
+				} ,
+				'unmatch': function( file, result ){
+					// console.log('unmatch: ' + file);
+					// console.log(result);
+				} ,
+				'error': function( file, error ){
+					// console.log('error: ' + file);
+					// console.log(result);
+				} ,
+				'complete': function(){
+					console.log('all done!');
+					assert.equal(1, 1);
+					done();
+				}
+			}
+		);
+
+		setTimeout(function(){
+			var canceled = searchInDir.cancel();
+			assert.ok(canceled);
+		}, 1500);
+
 	});
 
 });
